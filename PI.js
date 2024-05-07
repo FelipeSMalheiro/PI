0a2e768938a1f30ad203f5e7ade6e4ea0a5c71a9
Felipe S Malheiro <Felipes.malheiro@gmail.com>
2024-05-06 21:14:27 -0300

    gitignore e prisma

--- PI.js
+++ PI.js
@@ -1,2 +1,10 @@
-//projeto PI
+import express from 'express'
+const app = express()
+const PORT = 3000
+app.get('/',(req,res) => {
+    res.send('Hello World')
+})
+app.listen(PORT, () => {
+    console.log(`Server executando em http://localhost:${PORT}`)
+})
 
